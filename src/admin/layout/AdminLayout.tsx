f563db5594956bd13fd35b23edc654485c509937
JhonveraDev <seguridad.veracontainer@gmail.com>
2025-09-01 16:39:29 -0500

    refactor all elements

--- src/admin/layout/AdminLayout.tsx
+++
@@ -1,5 +0,0 @@
-export const AdminLayout = () => {
-  return (
-    <div>AdminLayout</div>
-  )
-}
--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,17 +1,23 @@
-import { createBrowserRouter, Navigate } from "react-router";
-import { ShopLayout } from "./shop/layaouts/ShopLayout";
-import { HomePage } from "./shop/pages/home/HomePage";
-import { ProductPage } from "./shop/pages/product/ProductPage";
-import { GenderPage } from "./shop/pages/gender/GenderPage";
-import { AuthLayout } from "./auth/layouts/AuthLayout";
-import { LoginPage } from "./auth/pages/login/LoginPage";
-import { RegisterPage } from "./auth/pages/register/RegisterPage";
-import { AdminLayout } from "./admin/layout/AdminLayout";
-import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
-import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
+import { lazy } from 'react';
+import { createBrowserRouter, Navigate } from 'react-router';
+
+import { ShopLayout } from './shop/layouts/ShopLayout';
+import { HomePage } from './shop/pages/home/HomePage';
+import { ProductPage } from './shop/pages/product/ProductPage';
+import { GenderPage } from './shop/pages/gender/GenderPage';
+
+import { LoginPage } from './auth/pages/login/LoginPage';
+import { RegisterPage } from './auth/pages/register/RegisterPage';
+
+import { DashboardPage } from './admin/pages/dashboard/DashboardPage';
+import { AdminProductPage } from './admin/pages/product/AdminProductPage';
+import { AdminProductsPage } from './admin/pages/products/AdminProductsPage';
+
+const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
+const AdminLayout = lazy(() => import('./admin/layouts/AdminLayout'));
 
 export const appRouter = createBrowserRouter([
-  //Main Routes
+  // Main routes
   {
     path: '/',
     element: <ShopLayout />,
@@ -27,49 +33,50 @@ export const appRouter = createBrowserRouter([
       {
         path: 'gender/:gender',
         element: <GenderPage />,
-      }
-    ]
+      },
+    ],
   },
-  //Auth Routes
+
+  // Auth Routes
   {
     path: '/auth',
     element: <AuthLayout />,
     children: [
       {
         index: true,
-        element: <Navigate to='/auth/login' />
+        element: <Navigate to="/auth/login" />,
       },
       {
         path: 'login',
-        element: <LoginPage />
+        element: <LoginPage />,
       },
       {
         path: 'register',
-        element: <RegisterPage />
-      }
-    ]
+        element: <RegisterPage />,
+      },
+    ],
   },
-  //Admin Routes
+  // Admin Routes
   {
     path: '/admin',
     element: <AdminLayout />,
     children: [
       {
         index: true,
-        element: <DashboardPage />
+        element: <DashboardPage />,
       },
       {
         path: 'products',
-        element: <AdminProductsPage />
+        element: <AdminProductsPage />,
       },
       {
         path: 'products/:id',
-        element: <ProductPage />
-      }
-    ]
+        element: <AdminProductPage />,
+      },
+    ],
   },
   {
     path: '*',
-    element: <Navigate to='/' />
-  }
-])
+    element: <Navigate to="/" />,
+  },
+]);
